fbc145dc3ea180f83b433bf15720d0b8df5fccfb
Rasyid Kusnady <54275103+rasyidk@users.noreply.github.com>
2023-12-21 22:17:32 +0700

    edit and berita

--- app/src/main/java/com/example/mobile_uas/ui/history/MenuUserAdapter.kt
+++ app/src/main/java/com/example/mobile_uas/ui/history/MenuUserAdapter.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
@@ -25,7 +26,8 @@ class MenuUserAdapter(var menuUserList: List<MenuUser>, private val menuUserDao:
         val textViewDate : TextView = itemView.findViewById(R.id.item_History_Date)
         val textViewType : TextView = itemView.findViewById(R.id.item_History_Type)
         val textServing : TextView = itemView.findViewById(R.id.item_History_Serving)
-        val btDelete : Button = itemView.findViewById(R.id.item_History_Delete);
+        val btDelete : Button = itemView.findViewById(R.id.item_History_Delete)
+        val btEdit : Button = itemView.findViewById(R.id.item_History_bt_edit)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
@@ -42,6 +44,67 @@ class MenuUserAdapter(var menuUserList: List<MenuUser>, private val menuUserDao:
         holder.textViewCalorie.text = "Calorie: ${currentItem.foodCalorie}"
         holder.textViewDate.text = currentItem.date
         holder.textServing.text = currentItem.serving.toString() + "g"
+
+
+        holder.btEdit.setOnClickListener {
+            val editDialogBuilder = AlertDialog.Builder(holder.itemView.context)
+            editDialogBuilder.setTitle("Edit Makanan")
+
+            val editDialogView = LayoutInflater.from(holder.itemView.context).inflate(R.layout.edit_dialog, null)
+
+            val editFoodName = editDialogView.findViewById<EditText>(R.id.editFoodName)
+            val editFoodCalorie = editDialogView.findViewById<EditText>(R.id.editFoodCalorie)
+
+            // Set the current values to the EditTexts
+            editFoodName.setText(currentItem.foodName)
+            editFoodCalorie.setText(currentItem.foodCalorie.toString())
+
+            editDialogBuilder.setView(editDialogView)
+
+            editDialogBuilder.setPositiveButton("Yes") { dialog, _ ->
+                // User clicked Yes, validate and update the food information
+                val newFoodName = editFoodName.text.toString().trim()
+                val newFoodCalorieStr = editFoodCalorie.text.toString().trim()
+
+                if (newFoodName.isNotEmpty() && newFoodCalorieStr.isNotEmpty()) {
+                    // Validation passed, proceed with updating
+                    val newFoodCalorie = newFoodCalorieStr.toInt()
+
+                    CoroutineScope(Dispatchers.IO).launch {
+                        // Update the MenuUser object with new values
+                        val updatedMenuUser = currentItem.copy(foodName = newFoodName, foodCalorie = newFoodCalorie)
+
+                        // Call the update method from the DAO
+                        menuUserDao.update(updatedMenuUser)
+
+                        withContext(Dispatchers.Main) {
+                            // Optionally, show a toast message to indicate successful update
+                            Toast.makeText(
+                                holder.itemView.context,
+                                "Item updated",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                    dialog.dismiss()
+                } else {
+                    // Validation failed, show an error message or handle it as needed
+                    Toast.makeText(
+                        holder.itemView.context,
+                        "Please enter valid values for food name and calorie",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+
+            editDialogBuilder.setNegativeButton("Cancel") { dialog, _ ->
+                // User clicked Cancel, do nothing
+                dialog.dismiss()
+            }
+
+            val editAlertDialog: AlertDialog = editDialogBuilder.create()
+            editAlertDialog.show()
+        }
 
         holder.btDelete.setOnClickListener{
 
